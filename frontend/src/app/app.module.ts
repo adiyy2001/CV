933fe5c10f064552c14fff2c047661b97d6c32da
adiyy2001 <adiy@onet.pl>
2021-01-22 22:43:20 +0100

    add header module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { HeaderModule } from './header/headers.module';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -12,7 +14,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     NgbModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HeaderModule
+  ],
+  exports: [
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
