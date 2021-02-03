361a036577dc1c04770f5040767d9b94d41ec8af
adiyy2001 <adiy@onet.pl>
2021-02-03 17:16:51 +0100

    add translations to heor typewriter

--- frontend/src/app/hero-part/hero-part.component.ts
+++ frontend/src/app/hero-part/hero-part.component.ts
@@ -2,6 +2,7 @@ import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { fromEvent } from 'rxjs';
 
+
 @Component({
   selector: 'app-hero-part',
   templateUrl: './hero-part.component.html',
@@ -11,6 +12,7 @@ export class HeroPartComponent implements OnInit {
   constructor(private translate: TranslateService) {}
   translationKeys = ['development', 'managment', 'cloud_solutions', 'user_interfaces'];
   list: string[]  = [];
+
   ngOnInit(): void {
     this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
       this.list = this.translationKeys.map((text) => this.translate.instant(text));
